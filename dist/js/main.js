e20f2c222fdd8d5ff8649fd05dc57c2a94f6ed7b
Your name <Your@emailaddres.com>
2020-09-27 00:00:20 +0200

    books

--- dist/js/main.js
+++
@@ -1,8 +0,0 @@
-
-$(function(){
-  'use strict';
-  var winH = $(window).height(),
-     uperH = $('.upper-bar').innerHeight(),
-     navH = $('.navbar').innerHeight();
-     $('.slider , .carousel-item').height(winH - ( uperH + navH + 50));
-});
